bc8bb9a0aeadc4decc06fae14f911be88fc9ffed
willmclellarn <willmclellarn@gmail.com>
2022-03-24 15:05:27 -0600

    first udemy commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (module.hot) {
+  module.hot.accept();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Will"
+        timeAgo="Today at 4:45PM"
+        commentText="I love it!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        commentText="Eh, it kinda stunk"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        commentText="My fav blog ever"
+        avatar={faker.image.image()}
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
